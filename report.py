6f63628600650e605aad288df9da2961a0ee8f0e
cabronita <cabronita@imap.cc>
2022-06-04 23:30:38 +0100

    First release

--- report.py
+++ report.py
@@ -6,11 +6,11 @@ import logging
 import pymongo
 
 parser = ArgumentParser()
-parser.add_argument('target', help='target hostname')
+parser.add_argument('targets', metavar='TARGET', nargs='+', help='target hostname')
 parser.add_argument('-v', '--verbosity', action='count', default=0, help='increase output verbosity (-vv for debug)')
 args = parser.parse_args()
 
-target = args.target
+targets = args.targets
 verbosity = args.verbosity
 
 logging_levels = [logging.WARNING, logging.INFO, logging.DEBUG]
@@ -20,32 +20,58 @@ logging.basicConfig(level=logging_levels[verbosity],
 
 client = pymongo.MongoClient("mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=rs0")
 database = 'arping'
-collection = target
-cursor = client[database][collection]
+
+
+def get_recent_online_times(cursor, earliest_timestamp):
+    online_times = []
+    records = cursor.find().sort('_id', pymongo.ASCENDING)
+    for record in records:
+        dt_time = datetime.strptime(record['_id'], '%Y-%m-%d %H:%M')
+        if dt_time >= earliest_timestamp:
+            online_times.append(dt_time)
+    return online_times
+
+
+def get_status_change_times(target):
+    cursor = client[database][target]
+    now = datetime.now().replace(second=0, microsecond=0)
+    earliest_timestamp = now - timedelta(days=6)
+    online_times_list = get_recent_online_times(cursor, earliest_timestamp)
+    if not online_times_list:
+        return
+
+    state_changes = list()
+    ts = earliest_timestamp
+    previous_online = None
+    while ts < now:
+        if ts in online_times_list:
+            online = True
+        else:
+            online = False
+        if online != previous_online:
+            state_changes.append({'ts': ts, 'online': online})
+        previous_online = online
+        ts += timedelta(minutes=1)
+    return state_changes
 
 
 def main():
-    records = cursor.find().sort('_id', pymongo.ASCENDING)
-    online_times_list = []
-    for i in records:
-        online_times_list.append(datetime.strptime(i['_id'], '%Y-%m-%d %H:%M'))
-    if not online_times_list:
-        print('No data')
-        exit()
-    dt_time = datetime.now().replace(second=0, microsecond=0) - timedelta(days=6)
-    previous_state = False
-    while True:
-        was_online = dt_time in online_times_list
-        if was_online == previous_state:
-            if was_online:
-                print(f"{dt_time.strftime('%a %H:%M')} UP")
-            else:
-                print(f"{dt_time.strftime('%a %H:%M')} DOWN")
-        if dt_time == datetime.now().replace(second=0, microsecond=0):
-            break
+    for target in targets:
+        print(target)
+        states = get_status_change_times(target)
+        if states:
+            for item in states:
+                state = 'UP' if item['online'] else 'DOWN'
+                print('-', item['ts'].strftime('%a %H:%M'), state)
         else:
-            dt_time = dt_time + timedelta(minutes=1)
-            previous_state = not was_online
+            print('- No records')
+        if not states:
+            color = 'grey'
+            ts = ''
+        else:
+            (ts, current_state) = states[-1].values()
+            color = 'GREEN' if item['online'] else 'grey'
+        logging.info(f"{color} {ts}")
 
 
 if __name__ == "__main__":
